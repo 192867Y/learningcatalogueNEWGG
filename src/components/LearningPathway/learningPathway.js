af2f240f3179612bde93d2d20c1ef30364d09026
GregoryT <59188159+192867Y@users.noreply.github.com>
2021-10-28 13:56:35 +0800

    Update learningPathway.js

--- src/components/LearningPathway/learningPathway.js
+++ src/components/LearningPathway/learningPathway.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PathwayDetails from "./pathwayDetails";
 import SearchBar from '../NavBar/searchBar';
+import axios from 'axios';
 
 const style = {
     color: '#455A64',
@@ -12,10 +13,26 @@ const style = {
 
 
 function LearningPathway(props) {
+
+    function componentDidMount() {
+        const api = 'https://o5shq9fg76.execute-api.us-east-1.amazonaws.com/staging';
+        const data = { "name" : "Gregory" };
+        axios
+          .post(api, data)
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        }
+  
+
     return (
 
         <div className="container">
-            <SearchBar />
+            <SearchBar /> <br/>
+            <button onClick={() => componentDidMount()}> Lambda Function Test </button>
             <hr />
             <h2 style={style}> Learning Pathway </h2>
             <hr />
